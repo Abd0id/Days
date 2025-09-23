c08d1047aa99f0dbea38ed338b1b9fc30d3fc5e2
Abd0id <abdallahidfat@gmail.com>
2025-09-23 17:11:27 +0100

    valeur du polynôme

---
+++ day1/project6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    float x, y;
+    printf("Entre une valeur de x: ");
+    scanf("%f", &x);
+
+    y = (((3 * (x + 2)) * (x - 5) * (x - 1) * (x + 7) * (x - 6)));
+
+    printf("\nLa resultat est : %.2f\n", y);
+
+    return 0;
+}
