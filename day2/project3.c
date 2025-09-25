de875467e4afb61d17bba5500f77fa14692cbc05
Abd0id <abdallahidfat@gmail.com>
2025-09-25 15:13:57 +0100

    project 3 fixed

--- day2/project3.c
+++ day2/project3.c
@@ -1,125 +1,58 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 
 int main() {
-// calculated times in minutes
-    int possible_minutes[] = {
-        8 * 60 + 0,    // "08:00"
-        9 * 60 + 43,   // "09:43"
-        11 * 60 + 19,  // "11:19"
-        12 * 60 + 47,  // "12:47"
-        14 * 60 + 0,   // "14:00"
-        15 * 60 + 45,  // "15:45"
-        19 * 60 + 0,   // "19:00"
-        21 * 60 + 45   // "21:45"
-    };
-//  for result
-    char possible_strings[8][6] = {
-        "08:00", "09:43", "11:19", "12:47",
-        "14:00", "15:45", "19:00", "21:45"
+    int departures[] = {
+        8 * 60 + 0,    // 08:00
+        9 * 60 + 43,   // 09:43
+        11 * 60 + 19,  // 11:19
+        12 * 60 + 47,  // 12:47
+        14 * 60 + 0,   // 14:00
+        15 * 60 + 45,  // 15:45
+        19 * 60 + 0,   // 19:00
+        21 * 60 + 45   // 21:45
     };
 
-    char heure[6];
-    
-    printf("Entrez une heure (HH:MM) : ");
-    scanf("%s", heure);
-
-    int hr, min;      
-    sscanf(heure, "%d:%d", &hr, &min);
-    
-    int user_minutes = hr * 60 + min;
-#include <stdio.h>
-
-int main()
-{
-    int depart[] = {
-        8 * 60 + 0,    //8:00
-        9 * 60 + 43,   //9:43
-        11 * 60 + 19,  //11:19
-        12 * 60 + 47,  //12:47
-        14 * 60 + 0,   //14:00
-        15 * 60 + 45,  //15:45
-        19 * 60 + 0,   //19:00
-        21 * 60 + 45   //21:45
+    int arrivals[] = {
+        10 * 60 + 16,  // 10:16
+        11 * 60 + 52,  // 11:52
+        13 * 60 + 31,  // 13:31
+        15 * 60 + 0,   // 15:00
+        16 * 60 + 8,   // 16:08
+        17 * 60 + 55,  // 17:55
+        21 * 60 + 20,  // 21:20
+        23 * 60 + 58   // 23:58
     };
 
-    int arrival[] = {
-        10 * 60 + 16,  //10:16
-        11 * 60 + 52,  //11:52
-        13 * 60 + 31,  //13:31
-        15 * 60 + 0,   //15:00
-        16 * 60 + 8,   //16:08
-        17 * 60 + 55,  //17:55
-        21 * 60 + 20,  //21:20
-        23 * 60 + 58   //23:58
+    char *depart_str[] = {
+        "8:00 a.m.", "9:43 a.m.", "11:19 a.m.", "12:47 p.m.",
+        "2:00 p.m.", "3:45 p.m.", "7:00 p.m.", "9:45 p.m."
     };
 
-    int heure, minutes, user_time;
-    int i, min_diff = 99999; 
-    int closest
-     = 0;
+    char *arrive_str[] = {
+        "10:16 a.m.", "11:52 a.m.", "1:31 p.m.", "3:00 p.m.",
+        "4:08 p.m.", "5:55 p.m.", "9:20 p.m.", "11:58 p.m."
+    };
 
-    printf("\nEntrer l'Heure de depart : ");
-    scanf("%d", &heure);
+    int hour, minute;
+    printf("Entrez une heure (24h) [HH:MM] : ");
+    scanf("%d:%d", &hour, &minute);
 
-    printf("\nEntrer Minutes de depart : ");
-    scanf("%d", &minutes);
+    int user_time = hour * 60 + minute;
 
-    user_time = heure * 60 + minutes;
+    int closest_index = 0;
+    int min_diff = abs(user_time - departures[0]);
 
-    printf("Heure saisie en minutes : %d\n", user_time);
-
-    for (i = 0; i < 8; i++) {
-
-        int diff = depart[i] - user_time;
-
-        if (diff < 0) diff = -diff;
-
+    for (int i = 1; i < 8; i++) {
+        int diff = abs(user_time - departures[i]);
         if (diff < min_diff) {
             min_diff = diff;
-            closest
-             = i;
+            closest_index = i;
         }
     }
 
-    int dep_heure = depart[closest
-    ] / 60;
-
-    int dep_min = depart[closest
-    ] % 60;
-
-    int arr_hour = arrival[closest
-    ] / 60;
-
-    int arr_min = arrival[closest
-    ] % 60;
-
-    printf("Le vol le plus proche part à : %02d:%02d\n", dep_heure, dep_min);
-    printf("Heure d'arrivée correspondante : %02d:%02d\n", arr_hour, arr_min);
-
-    return 0;
-}
-    int min_cmp = 24 * 60 + 1; 
-    int closest
-     = -1;
-    
-    for (int i = 0; i < 8; i++) {
-        int difference = abs(user_minutes - possible_minutes[i]);
-        
-        if (difference < min_cmp) {
-            min_cmp = difference;
-            closest
-             = i;
-        }
-    }
-
-    if (closest
-         != -1) {
-        printf("L'heure la plus proche de %s est %s.\n", heure, possible_strings[closest]);
-    } else {
-        printf("Une erreur est survenue lors de la recherche de l'heure la plus proche.\n");
-    }
+    printf("L'heure de départ la plus proche est %s, arrivant à %s\n",
+           depart_str[closest_index], arrive_str[closest_index]);
 
     return 0;
 }
